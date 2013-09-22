aa644047ea7410b12ef447a8a758b9a06867f25d
Russell Duhon <fugu13@gmail.com>
2013-09-22 16:11:26 -0700

    check multimarkdown works

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
+import subprocess
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return subprocess.check_output(['multimarkdown', '-h'])
 
 
